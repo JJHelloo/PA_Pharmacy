f9d4093088fc76086aef4d44392ff58972f81abd
RyanCSUMB <>
2022-02-11 09:21:54 +0000

    corrections to Manager Report and FDAReport sql search using 'like' and dateIssued instead of dateFilled

--- FDAreport.java
+++ FDAreport.java
@@ -3,6 +3,7 @@
 //Output result table to Console
 
 package com.csumb.cst363;
+
 import java.sql.*;
 import java.util.Scanner;
 
@@ -10,45 +11,43 @@ import java.util.Scanner;
 An FDA government official is looking for the quantity of drugs that each doctor has prescribed.  The report shows the doctor’s name and quantity prescribed.  Input is drug name (may be partial name) and a start and end date range.
 */
 
-public class FDAreport {
+public class FDAreport
+{
+   public static void main(String[] args)
+   {
 
-public static void main(String[] args) {
-	
-	//GET USER INPUT FOR REPORT
-	Scanner input = new Scanner(System.in);  // Reading from System.in
-    System.out.println("Enter a Drug: ");
-    String DrugIn = input.next();
-    System.out.println("Prescribed between (Enter search Start Date YYY-MM-DD: ");
-    String StartDate = input.next();
-    System.out.println("Prescribed between (Enter search End Date YYY-MM-DD: ");
-    String EndDate = input.next();
-    
-    input.close(); 
-    
-        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pa_pharmacy", "root", "root"); ) {
-///////
+      // GET USER INPUT FOR REPORT
+      Scanner input = new Scanner(System.in); // Reading from System.in
+      System.out.println("Enter a Drug: ");
+      String DrugIn = input.next();
+      System.out.println("Prescribed between (Enter search Start Date) YYYY-MM-DD: ");
+      String StartDate = input.next();
+      System.out.println("Prescribed between (Enter search End Date) YYYY-MM-DD: ");
+      String EndDate = input.next();
 
-PreparedStatement ps = con.prepareStatement("select dr.name, sum(rx.quantity) from doctor dr join rx on dr.id = rx.Doctor_DoctorID join drug d on rx._drugID = d.DrugID where d.TradeName =? AND rx.datefilled >? AND rx.datefilled <? group by dr.name");
-			ps.setString(1, DrugIn);
-			ps.setString(2, StartDate);
-			ps.setString(3, EndDate);
-			
-			//ps.setString(2, "2022-02-07");
-			//ps.setString(3, "2000-01-01");
-			
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                String DocName = rs.getString(1);
-                int Sum = rs.getInt(2);
-             
-                System.out.printf("%-20s %-20s \n", DocName, Sum);
-            }
+      input.close();
 
+      try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pa_pharmacy", "root",
+            "root");)
+      {
+         PreparedStatement ps = con.prepareStatement(
+               "select dr.name, sum(rx.quantity) from doctor dr join rx on dr.id = rx.Doctor_DoctorID join drug d on rx._drugID = d.DrugID where d.TradeName like ? AND rx.dateIssued > ? AND rx.dateIssued < ? group by dr.name");
+         ps.setString(1, "%" + DrugIn + "%");
+         ps.setString(2, StartDate);
+         ps.setString(3, EndDate);
 
-//////
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-    }
+         ResultSet rs = ps.executeQuery();
+         while (rs.next())
+         {
+            String DocName = rs.getString(1);
+            int Sum = rs.getInt(2);
 
+            System.out.printf("%-20s %-20s \n", DocName, Sum);
+         }
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+   }
 }
--- ManagerReport.java
+++ ManagerReport.java
@@ -11,43 +11,46 @@ import java.util.Scanner;
 
 /*
 A pharmacy manager requests a report of the quantity of drugs that have been used to fill prescriptions by the pharmacy.  The report will contain the names of drugs used and the quantity of each drug used. Input is pharmacy id and a start and end date range.
-*/
+ */
 
-public class ManagerReport {
+/**/
+public class ManagerReport
+{
+   public static void main(String[] args)
+   {
 
-	public static void main(String[] args) {
-		
-		//GET USER INPUT FOR REPORT
-		Scanner input = new Scanner(System.in);  // Reading from System.in
-	    System.out.println("Enter a Pharmacy ID: ");
-	    String PharmacyId = input.next();
-	    System.out.println("Prescribed between (Enter search Start Date YYY-MM-DD: ");
-	    String StartDate = input.next();
-	    System.out.println("Prescribed between (Enter search End Date YYY-MM-DD: ");
-	    String EndDate = input.next();
-	    
-	    input.close(); 
-	    
-	        try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pa_pharmacy", "root", "root"); ) {
-	///////
+      // GET USER INPUT FOR REPORT
+      Scanner input = new Scanner(System.in); // Reading from System.in
+      System.out.println("Enter a Pharmacy ID: ");
+      String PharmacyId = input.next();
+      System.out.println("Prescribed between (Enter search Start Date) YYYY-MM-DD: ");
+      String StartDate = input.next();
+      System.out.println("Prescribed between (Enter search End Date) YYYY-MM-DD: ");
+      String EndDate = input.next();
 
-	PreparedStatement ps = con.prepareStatement("select d.tradename, sum(rx.quantity) from rx join drug d on rx._drugid = d.drugid where rx.pharmacy_id =? AND datefilled BETWEEN ? AND ? group by tradename");
-				ps.setString(1, PharmacyId);
-				ps.setString(2, StartDate);
-				ps.setString(3, EndDate);
+      input.close();
 
-	            ResultSet rs = ps.executeQuery();
-	            while (rs.next()) {
-	                String DrugName = rs.getString(1);
-	                int Sum = rs.getInt(2);
-	             
-	                System.out.printf("%-20s %-20s \n", DrugName, Sum);
-	            }
+      try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pa_pharmacy", "root",
+            "root");)
+      {
+         PreparedStatement ps = con.prepareStatement(
+               "select d.tradename, sum(rx.quantity) from rx join drug d on rx._drugid = d.drugid where rx.pharmacy_id = ? AND datefilled BETWEEN ? AND ? group by tradename");
+         ps.setString(1, PharmacyId);
+         ps.setString(2, StartDate);
+         ps.setString(3, EndDate);
 
+         ResultSet rs = ps.executeQuery();
+         while (rs.next())
+         {
+            String DrugName = rs.getString(1);
+            int Sum = rs.getInt(2);
 
-	//////
-	        } catch (Exception e) {
-	          e.printStackTrace();
-	        }
-	    }
-} 
+            System.out.printf("%-20s %-20s \n", DrugName, Sum);
+         }
+
+      } catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
